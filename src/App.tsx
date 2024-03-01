a7010053cef360c3d8df34a3ac606c231d7ba032
Alessandro Cipolletti <cipolletti.alessandro@gmail.com>
2024-03-01 21:21:22 +0100

    Get rid of boilerplate code

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
 import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import styled from 'styled-components'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <PageWrapper>
+      <Header>
+        <h2>React ts map from scratch</h2>
+      </Header>
+      <MainWrapper>
+        <MapWrapper>
+
+        </MapWrapper>
+        <Sidebar>
+
+        </Sidebar>
+      </MainWrapper>
+    </PageWrapper>
   )
 }
+
+const PageWrapper = styled.div`
+  padding: 10px;
+`
+const Header = styled.header`
+  text-align: center;
+`
+const MainWrapper = styled.main`
+  width: 100%;
+  display: flex;
+  gap: 10px;
+`
+const MapWrapper = styled.div`
+  width: calc(100% - 320px);
+  min-height: 80vh;
+  background-color: yellow;
+`
+const Sidebar = styled.aside`
+  width: 300px;
+  background-color: #f4f4f4;
+`
 
 export default App
